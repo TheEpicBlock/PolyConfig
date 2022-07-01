9ae29595ab3933fa5673f9aa586be3f126ee5eec
TheEpicBlock <git.teb@theepicblock.nl>
2022-07-01 17:32:24 +0200

    Include all dependencies and stuff

--- src/main/java/nl/theepicblock/polyconfig/PolyConfig.java
+++ src/main/java/nl/theepicblock/polyconfig/PolyConfig.java
@@ -5,14 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PolyConfig implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("polyconfig");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
 	}
 }
